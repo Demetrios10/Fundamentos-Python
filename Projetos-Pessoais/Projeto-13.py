9396668616853252660db204a346f83d4ab5c924
Demetrios10 <demetrios.10@hotmail.com>
2026-01-01 19:44:46 -0300

    Au

--- Projetos-Pessoais/Projeto-13.py
+++ Projetos-Pessoais/Projeto-13.py
@@ -1,14 +1,18 @@
 # Lista de alunos em ordem alfabetica 
 
+# Lista Vazia 
 alunos = []
 
+# Laço executa 5 vezes esse trecho do codigo
 for i in range(5):
     print('\nDigite os Nomes dos Alunos: ')
     aluno = input()
     alunos.append(aluno)
 
+# Classifica em ordem alfabetica 
 alunos.sort()
 
+# ordena em ordem alfabetica 
 print(f'\nA Lista dos alunos do 3 ano em ordem alfabetica é: ')
 for aluno in alunos:
     print(aluno)
--- Projetos-Pessoais/Projeto-14.py
+++ Projetos-Pessoais/Projeto-14.py
@@ -1,14 +1,18 @@
 # Cores do arco iris 
 
+# Lista Vazia 
 cores_arco_iris = []
 
+# Laço executa 5 vezes esse trecho do codigo
 for i in range(4):
     print('Digite as cores do seu arco iris: ')
     cores = input()
     cores_arco_iris.append(cores)
 
+# Classifica em ordem alfabetica 
 cores_arco_iris.sort()
 
+# ordena em ordem alfabetica 
 print(f'As Cores do arco iris em ordem são: ')
 for cores in cores_arco_iris:
     print(cores)
