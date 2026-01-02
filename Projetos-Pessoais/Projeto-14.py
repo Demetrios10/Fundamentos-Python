fec97969ccecd3e8e68162d2830b18aca4b28fcc
Demetrios10 <demetrios.10@hotmail.com>
2026-01-02 17:58:57 -0300

    teste

---
+++ Projetos-Pessoais/Projeto-14.py
@@ -0,0 +1,13 @@
+# Iterando Strings com while 
+
+nome = input('Digite um Nome: ')  # Iteraveis 
+tamanho_nome = len(nome)
+
+indice = 0
+novo_nome = ''
+while indice < len(nome):
+    letra = (nome[indice])
+    novo_nome += f'*{letra}'
+    indice += 1
+
+print(novo_nome)
