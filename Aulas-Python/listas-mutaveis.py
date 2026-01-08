f795cecb589a1f9d2ccab75048ce7120abbe266f
Demetrios10 <demetrios.10@hotmail.com>
2026-01-07 23:45:40 -0300

    update

---
+++ Aulas-Python/listas-mutaveis.py
@@ -0,0 +1,10 @@
+
+lista = [123,True,'Demetrios',1.2,[]] # string vazia
+print(lista) 
+print(lista[3]) # indice 3
+print(lista[4]) # indice 4 
+print(lista[-2]) # indice -2
+print(lista[2], type(lista[2])) # indice 2 tipo str
+lista [-2] = 'Deltas' # subistituindo o indice -2 por Deltas
+print(lista)
+
