4c0ccc6b8ace42cb7f328b64b59e77428bd52f86
Demetrios10 <demetrios.10@hotmail.com>
2026-01-08 00:46:32 -0300

    Up

---
+++ Projetos-Pessoais/Projeto-17.py
@@ -0,0 +1,13 @@
+# Exiba os índices da lista 
+# 0 Demetrios
+# 1 Deltas
+# 2 Maria
+# 3 Joao
+
+nomes = ['Demetrios','Deltas','Maria','Joao']
+indices = range(len(nomes))
+
+for indice in indices:
+    print(indice,nomes[indice])
+
+ 
