b7b90a1597cc3899a4bb293838d5e75ae0b2f259
Demetrios10 <demetrios.10@hotmail.com>
2026-01-14 22:46:21 -0300

    Update

--- Aulas/dicionarios.py
+++ Aulas/dicionarios.py
@@ -1,15 +1,27 @@
 # Dicionário 
 
-elemento = {
-    'Z':3,
-    'nome':'Lítio',
-    'grupo':'Metais Alcalinos',
-    'densidade':0.534
+# elemento = {
+#     'Z':3,
+#     'nome':'Lítio',
+#     'grupo':'Metais Alcalinos',
+#     'densidade':0.534
+# }
+
+# print(f'Elemneto: {elemento['nome']}') # printando somente um elemento na saida de dados 
+# print(f'Densidade: {elemento['densidade']}') # printando somente um elemento na saida de dados 
+# print(f'O dicionario possui {len(elemento)} elementos') # contando quantos elementos possuem no dicionario
+
+# elemento ['grupo'] = 'Deltas' # Trocando um elemento no dicionario 
+# print(elemento)
+
+
+
+nomes = {
+
+    'nome 1':'Demetrios',
+    'nome 2': 'Joana',
+    'nome 3':'Matheus',
+    'nome 4':'Gustavo'
 }
-
-print(f'Elemneto: {elemento['nome']}') # printando somente um elemento na saida de dados 
-print(f'Densidade: {elemento['densidade']}') # printando somente um elemento na saida de dados 
-print(f'O dicionario possui {len(elemento)} elementos') # contando quantos elementos possuem no dicionario 
-
-elemento ['grupo'] = 'Deltas' # Trocando um elemento no dicionario 
-print(elemento)
+print(nomes)
+print(f'nomes 1:{nomes['nome 1']}')
