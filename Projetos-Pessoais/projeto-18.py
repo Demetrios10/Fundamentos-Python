f61a3c92fe8779814392f97a495cbd8240bee314
Demetrios10 <demetrios.10@hotmail.com>
2026-01-15 10:37:12 -0300

    Update

---
+++ Projetos-Pessoais/projeto-18.py
@@ -0,0 +1,12 @@
+# Novo Projeto 
+
+
+while True:
+ nome = input("Digite seu nome: ")
+ if nome != "Demetrios":
+  print(f'Esse Nome {nome} Esta Errado !!')
+ else:
+  print(f'Nome {nome} Correto!!')
+  break
+
+ print("Digite Novamente Por favor")
