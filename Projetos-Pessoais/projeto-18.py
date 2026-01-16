34a05581833af4154c8d4a7c7f9e44b42f527c44
Demetrios10 <demetrios.10@hotmail.com>
2026-01-16 13:08:06 -0300

    commit

--- Projetos-Pessoais/projeto-18.py
+++ Projetos-Pessoais/projeto-18.py
@@ -1,12 +1,23 @@
-# Novo Projeto 
-
+# Inscrição de Cliente Exclusivo
+print('INSCRIÇÃO DE CLIENTE EXCLUSIVO')
 
 while True:
  nome = input("Digite seu nome: ")
  if nome != "Demetrios":
-  print(f'Esse Nome {nome} Esta Errado !!')
+  print('Nome Errado Por Favor tente novamente!!!!')
+  continue
  else:
-  print(f'Nome {nome} Correto!!')
-  break
-
- print("Digite Novamente Por favor")
+  print(f'Seja bem vindo {nome} !!')
+ if nome == "Demetrios":
+  print(f'{nome} Vamos Fazer sua inscrição ?')
+  fazer_cadastro = input('Digite [S]im ou [N]ão:')
+  if fazer_cadastro == "Sim" or fazer_cadastro == "sim":
+   print('Digite Seus Dados')
+  else:
+   print('voce saiu do sistema!')
+   break
+ nome_completo = input('Digite seu nome completo:')
+ cpf = input('Digite seu cpf:')
+ tel = input('Digite seu tel:')
+ print('Inscrição Realizada com Sucesso!!')
+ break 
