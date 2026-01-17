9c854bb06472a3f81a5621853073f6cc8a2b7c6f
Demetrios10 <demetrios.10@hotmail.com>
2026-01-17 19:58:09 -0300

    Up

--- Aulas-Primeira-Fase/dicionarios.py
+++ Aulas-Primeira-Fase/dicionarios.py
@@ -6,25 +6,28 @@ frutas = {
     'fruta 3': 'Goiaba',
     'fruta 4': 'Melancia'
 }
-# print(f'A Fruta é: {frutas['fruta 3']}') # mostra a fruta de numero 3 do dicionário
-# print(f'Nós Temos: {len(frutas)} frutas no total!!') # verifica quel total de itens tem no dicionário
+print(f'A Fruta é: {frutas['fruta 3']}') # mostra a fruta de numero 3 do dicionário
+print(f'Nós Temos: {len(frutas)} frutas no total!!') # verifica quel total de itens tem no dicionário
 
-# atualizar uma entrada
-frutas['fruta 4'] = 'uva'
-print(frutas)
+# # atualizar uma entrada
+# frutas['fruta 4'] = 'uva'
+# print(frutas)
 
-# Adicionar uma entrada
-frutas['Fruta 5'] = 'jaboticaba'
-print(frutas)
+# # Adicionar uma entrada
+# frutas['Fruta 5'] = 'jaboticaba'
+# print(frutas)
 
-# Exclusão de itens em dicionários
-del frutas['fruta 4']
-print(frutas)
+# # Exclusão de itens em dicionários
+# del frutas['fruta 4']
+# print(frutas)
 
-# Apagar todos os itens 
-frutas.clear()
-print(frutas)
+# # Apagar todos os itens 
+# frutas.clear()
+# print(frutas)
 
-# Apaga todo o dicionário da memoria 
-del frutas
-print(frutas)
+# # Apaga todo o dicionário da memoria 
+# del frutas
+# print(frutas)
+
+for i,j in frutas.items():
+    print(f'{i}: {j}')
