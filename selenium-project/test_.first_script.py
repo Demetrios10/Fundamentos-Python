07c636a9446d321bb61883ed9525689d5c17d7de
Demetrios10 <demetrios.10@hotmail.com>
2026-01-21 18:00:15 -0300

    Up

--- selenium-project/test_.first_script.py
+++ selenium-project/test_.first_script.py
@@ -1,8 +1,23 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.action_chains import ActionChains
 import time
-from selenium import webdriver # type: ignore
 
+# Inicializa o driver (ex: Chrome)
 driver = webdriver.Chrome()
-driver.get("http://selenium.dev")
-time.sleep(3)
 
-driver.quit()
+try:
+    # 2. Navegar até a página
+    driver.get("https://bin.qa.portaldocliente.fiserv.com/")
+    time.sleep(10)
+
+    botao_login = driver.find_element(By.ID,"b2-b2-b4-InputMask")
+    
+    # 4. Clicar no elemento
+    botao_login.click()
+    time.sleep(5)
+
+    # Salva o screenshot no diretório atual
+    driver.save_screenshot("evidencia_final.png")
+finally:
+    driver.quit()
