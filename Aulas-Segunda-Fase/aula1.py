4cde7443765498b440248f51c2318050ffe37c06
Demetrios10 <demetrios.10@hotmail.com>
2026-02-01 21:01:10 -0300

    Up

---
+++ Aulas-Segunda-Fase/aula1.py
@@ -0,0 +1,17 @@
+"""
+Docstring 
+multiplas linhas
+"""
+
+'''
+Tb pode ser usado 
+para escrever em 
+multiplas linhas 
+'''
+
+
+# esse é um comentario de uma linha 
+print('Demetrios') # comente a frente do código
+# ou comente abaixo do código 
+print(123)
+    
