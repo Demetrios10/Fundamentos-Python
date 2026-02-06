9d4e0fbe976fcb37ba4a7a0156d2a233b6fc4e7b
Demetrios10 <demetrios.10@hotmail.com>
2026-02-05 22:42:49 -0300

    Update

--- Aulas-Segunda-Fase/aula1.py
+++ Aulas-Segunda-Fase/aula1.py
@@ -4,3 +4,20 @@ print(123) # podemos escrever a frente
 
 # ou abaixo do código 
 
+print('Demetrios Alves Da Silva')
+
+
+"""
+DocString pode escrever 
+varias linhas de comentarios aqui 
+Dentro 
+"""
+
+
+'''
+Esse Tb é uma DocString
+tb pode ser usado para escrever suas 
+Notas ou códigos 
+aqui dentro 
+'''
+
