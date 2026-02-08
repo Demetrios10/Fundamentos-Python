1f41bac1c3416803842ff13d469bafd171a4b68e
Demetrios10 <demetrios.10@hotmail.com>
2026-02-08 12:33:56 -0300

    Update

--- Aulas-Segunda-Fase/aula3.py
+++ Aulas-Segunda-Fase/aula3.py
@@ -8,8 +8,13 @@ print('Boa Noite') # aspas simples
 print("Boa noite , tudo bem ?") # aspas duplas 
 print('Demetrios \"Alves\"') # caracter de escape
 print('"Demetrios" "Da Silva"') # aspas simples pode dentro de aspas duplas 
+print("'Oi aqui temos aspas simples dentro de aspas duplas'")
 
 print(123456789) # isso não é string 
+
+print('Todo texto vai dentro de aspas seja elas simples')
+print("Todo texto vai dentro de aspas seja sejam elas duplas")
+
 
 
 
---
+++ Aulas-Segunda-Fase/aula4.py
@@ -0,0 +1,16 @@
+# tipos int e float
+
+# int -> numero inteiro 
+print(11)
+print(-12)
+print(type(2026))
+
+
+# float -> numeros com ponto flutuante 
+print(1.1,10.11)
+print(type(1.200))
+print(type(1.5)) 
+
+
+# tipo string 
+print(type('Demetrios'))
