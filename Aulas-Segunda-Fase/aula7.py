53c35130499d12284e75b8c19760ddcdac110bed
Demetrios10 <demetrios.10@hotmail.com>
2026-02-09 22:29:11 -0300

    Update

---
+++ Aulas-Segunda-Fase/aula7.py
@@ -0,0 +1,17 @@
+# variaveis 
+
+nome_completo = 'Demetrios Alves Da Silva'
+soma_dois_mais_dois = 2 + 2
+numero_inteiro = 1
+
+print(numero_inteiro)
+print(nome_completo) 
+print(soma_dois_mais_dois)
+
+nome = 'Jonas'
+idade = 30
+maior_de_idade = idade >= 18
+
+print('Nome:' ,nome_completo, 'tem a idade de:' ,idade)
+print('É maior de idade: ',maior_de_idade)
+
---
+++ exercicios/exe1.py
@@ -0,0 +1,15 @@
+# exercicio
+
+nome = 'Joao'
+sobrenome = 'Da Silva'
+idade = 38
+ano_nascimento = 1986
+maior_de_idade = idade >= 18
+altura = 1.90
+
+print(f'Nome: {nome}')
+print(f'sobrenome: {sobrenome}')
+print(f'idade: {idade}')
+print(f'ano nascimento: {ano_nascimento}')
+print(f'É maior de idade ? {maior_de_idade}')
+print(f'altura: {altura}')
