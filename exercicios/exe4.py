4350ffe6428e60cd25ca09564c96e817bdb131d4
Demetrios10 <demetrios.10@hotmail.com>
2026-02-13 10:11:31 -0300

    Update

---
+++ exercicios/exe4.py
@@ -0,0 +1,9 @@
+# Jogo que adivinha as letras nas cores digitadas 
+
+cor = input('Digite uma cor: ')
+letra = input('Digite uma letra: ')
+
+if letra in cor:
+    print(f'{letra} esta em {cor}')
+else:
+    print(f'{letra} não esta em {cor}')
