32fe1ee458abcec634272a395dc8c1e58d64cb75
Demetrios10 <demetrios.10@hotmail.com>
2026-02-13 10:16:35 -0300

    up

--- exercicios/exe2.py
+++ exercicios/exe2.py
@@ -4,10 +4,6 @@ nome = 'Demetrios Alves'
 altura = 1.90
 peso = 100
 imc = peso / (altura * altura)
-salario = 10000
-data_nascimento = '19/12/1986'
+
 
 print(f'Meu Nome é {nome} eu tenho {altura:.2f} de altura e peso {peso} Kilos e o meu IMC esta em {imc}')
-
-# um exemplo com , 
-print(f'O salario do porteiro esta em {salario:,.2f} e ele nasceu em {data_nascimento}')
