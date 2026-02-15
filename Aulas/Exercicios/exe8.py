7fd89b269d429e79e7b87f3c124f5f3fbf6f5873
Demetrios10 <demetrios.10@hotmail.com>
2026-02-15 17:09:14 -0300

    Update

--- Aulas/Exercicios/exe8.py
+++ Aulas/Exercicios/exe8.py
@@ -1,8 +1,15 @@
-hora = int(input('Que horas são: '))
+hora = (input('Digite uma hora em numeros inteiros: '))
 
-if hora >= 0 and hora <= 11:
-    print('Bom dia')
-elif hora >= 12 and hora <= 17:
-    print('Boa tarde')
-else:
-    print('Boa noite')
+try:
+    horas = int(hora)
+    
+    if horas >= 0 and horas <= 11:
+        print('Bom dia')
+    elif horas >= 12 and horas <= 17:
+        print('Boa tarde')
+    elif horas >= 18 and horas <= 23:
+        print('Boa noite')
+    else:
+        print('não conheço essa hora')
+except:
+    print('Por favor,digite apenas numeros inteiros')
