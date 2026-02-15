19b68d3050c05435494df3c012f7483b93d6a9d9
Demetrios10 <demetrios.10@hotmail.com>
2026-02-15 15:51:30 -0300

    Up

--- Aulas/Fase-2/aula25.py
+++ Aulas/Fase-2/aula25.py
@@ -6,15 +6,25 @@ id = Identidade
 """
 
 
-v1 = 'a'
-v2 = 'c'
-print(id(v1))
-print(id(v2))
+# v1 = 'a'
+# v2 = 'c'
+# print(id(v1)) # identidade 
+# print(id(v2))
 
 
-condicao = False
+
+condicao = True # True ou False
+passou_no_if = None
+
 
 if condicao:
+    passou_no_if = True
     print('Faça algo')
 else:
     print('Não faça algo')
+    
+
+if passou_no_if is None:
+    print('Não passou no if')
+else:
+    print('Passou no if')
