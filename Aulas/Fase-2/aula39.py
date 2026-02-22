f4f363ded8d1dfa9fe2ded14b2e09da6ea82a882
Demetrios10 <demetrios.10@hotmail.com>
2026-02-22 13:24:59 -0300

    Update

---
+++ Aulas/Fase-2/aula39.py
@@ -0,0 +1,16 @@
+"""
+Listas em Python 
+Tipo list - Mutável 
+Suporta vários valores de qualquer tipo 
+Conhecimentos reutilizaveis - índices e fatiamento 
+Métodos úteis:  append , insert , pop , del , clear , extend , + 
+Create , ler , alterar , apagar = lista [i] (CRUD)
+"""
+
+
+lista = [10,20,30,40,50] # criar 
+lista[2] = 1000 # alterar 
+del lista[1] # apaga indice 2
+lista.append(2000) # adiciona 2000 ao final da lista 
+
+print(lista)
