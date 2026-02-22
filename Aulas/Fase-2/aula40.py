4e007521a79436b65729067d136faa0ef5f46e9b
Demetrios10 <demetrios.10@hotmail.com>
2026-02-22 17:02:28 -0300

    Up

--- Aulas/Fase-2/aula40.py
+++ Aulas/Fase-2/aula40.py
@@ -5,3 +5,12 @@ print(lista)
 
 lista.pop()
 print(lista)
+
+del lista[-1] 
+print(lista)
+
+lista.clear() # limpa lista 
+print(lista)
+
+lista.insert(0,5) # adiciona um item no indice 0
+print(lista)
