46eb00547ca74d073ec27f1de00aa114c365f83a
Demetrios10 <demetrios.10@hotmail.com>
2026-02-22 16:40:12 -0300

    Update

--- Aulas/Fase-2/aula39.py
+++ Aulas/Fase-2/aula39.py
@@ -11,6 +11,16 @@ Create , ler , alterar , apagar = lista [i] (CRUD)
 lista = [10,20,30,40,50] # criar 
 lista[2] = 1000 # alterar 
 del lista[1] # apaga indice 2
-lista.append(2000) # adiciona 2000 ao final da lista 
 
-print(lista)
+ # append adiciona ao final da lista 
+lista.append(2000)
+lista.append(900)
+lista.append(500)
+lista.append(700)
+
+lista.pop() # remove o ultimo item da lista 
+
+print(lista) # printa toda a lista 
+print(lista[1]) # printa indice 1 da lista 
+print(lista[0]) # printa indice 0 da lista 
+
---
+++ Aulas/Fase-2/aula40.py
@@ -0,0 +1,7 @@
+lista = [20,30,40,50]
+
+lista.append('Demetrios')
+print(lista)
+
+lista.pop()
+print(lista)
