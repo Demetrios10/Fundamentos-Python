80b16daa842d153f878d0c25a5b4df16bfceb09c
Demetrios10 <demetrios.10@hotmail.com>
2026-03-04 22:53:32 -0300

    up

--- Curso/Aulas-Fase-2/aula1.py
+++ Curso/Aulas-Fase-2/aula1.py
@@ -1,4 +1,11 @@
 # começando com o basico
 
+print('primeiro programa')
 
+print(1 + 2)
+print(2 - 1)
+print(3 * 1)
+print(3 ** 2)
+print(3 / 1)
 
+print('Aqui dentro eu posso escrever o que eu quiser !!')
