ff949a04c37d71706a6732f80bd955946485b47c
Demetrios Alves <demetrios.10@hotmail.com>
2026-05-03 01:28:52 -0300

    update

--- Automação-Site-Amazon/acessando-site-amazon.py
+++ Automação-Site-Amazon/acessando-site-amazon.py
@@ -11,8 +11,13 @@ driver.get("https://www.amazon.com.br")
 time.sleep(5)
 
 # clicando no botão todos
-botao_livros = driver.find_element(By.ID,"nav-hamburger-menu")
-botao_livros.click()
+botao_todos = driver.find_element(By.ID,"nav-hamburger-menu")
+botao_todos.click()
+time.sleep(5)
+
+# clicando no botão X para fechar
+botao_x = driver.find_element(By.ID,"hmenu-close-icon")
+botao_x.click()
 time.sleep(3)
 
 
