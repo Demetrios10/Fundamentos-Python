ed36fb18fb4948809ae9e46e56d8d37d87e5febf
Demetrios Alves <demetrios.10@hotmail.com>
2026-05-03 01:05:25 -0300

    update

---
+++ Automação-Site-Amazon/acessando-site-amazon.py
@@ -0,0 +1,18 @@
+# Acessando Site Amazon
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+import time
+
+# Inicializa o driver (ex: Chrome)
+driver = webdriver.Chrome()
+driver.get("https://www.amazon.com.br")
+time.sleep(5)
+
+# clicando no botão todos
+botao_livros = driver.find_element(By.ID,"nav-hamburger-menu")
+botao_livros.click()
+time.sleep(3)
+
+
