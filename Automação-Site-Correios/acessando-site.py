3cc5245feb1847454eda6ad3ab689f0f77eaa180
Demetrios Alves <demetrios.10@hotmail.com>
2026-05-03 00:23:06 -0300

    update

---
+++ Automação-Site-Correios/acessando-site.py
@@ -0,0 +1,21 @@
+# Acessando Site Correios
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+import time
+
+# Inicializa o driver (ex: Chrome)
+driver = webdriver.Chrome()
+driver.get("https://www.correios.com.br/")
+time.sleep(5)
+
+# clicando no botão enviar
+botao_enviar = driver.find_element(By.ID,"itemMenu_enviar")
+botao_enviar.click()
+time.sleep(5)
+
+# clicando no botão 
+botao_receber = driver.find_element(By.ID, "itemMenu_receber")
+botao_receber.click()
+time.sleep(5)
