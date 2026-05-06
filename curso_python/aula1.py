cd497f30ea136271c7980be24200262a6f375def
Demetrios Alves <demetrios.10@hotmail.com>
2026-05-05 22:24:13 -0300

    update

---
+++ curso_python/aula1.py
@@ -0,0 +1,9 @@
+# comentarios em python
+
+
+print('Hello World !') # comentario a frente 
+print('Demetrios')
+print('isso é python')
+# comentario abaixo 
+
+    
---
+++ curso_python/aula3.py
@@ -0,0 +1,7 @@
+# Tipos de dados 
+
+
+
+
+
+    
