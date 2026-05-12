1491a26a4aebde1a1bfe6f1d774e6974f0d81eb6
Demetrios Alves <demetrios.10@hotmail.com>
2026-05-12 16:18:48 -0300

    update

--- curso_python/aula8.py
+++ curso_python/aula8.py
@@ -1,4 +1,4 @@
-# exercicio de variaveis
+# exercicio de variaveis , declaracão e atribuição de variaveis
 
 nome = "Demetrios"
 sobrenome = "Alves Da Silva"
@@ -6,9 +6,21 @@ idade = 39
 ano_nascimento = 1986
 maior_de_idade = True
 altura_em_metros = 1.75
+cidade_natal = "São Paulo"
+nome_dos_pais = "Valquiria e Antonio"
+maior_de_idade = idade >= 18
+nome_carro_favorito = "Prsche 911"
+comida_favorita = "Pizza"
+cor_favorita = "Azul"
 
 print("O nome completo é " + nome + " " + sobrenome)
 print("Ele tem " + str(idade) + " anos")
 print("Ele nasceu em " + str(ano_nascimento))
 print("Ele é maior de idade: " + str(maior_de_idade))
 print("Ele mede " + str(altura_em_metros) + " metros")
+print("Ele nasceu na cidade de " + cidade_natal)
+print("O nome dos pais dele são " + nome_dos_pais)
+print("Ele é maior de idade ? " + str(maior_de_idade))
+print("O carro favorito dele é " + nome_carro_favorito)
+print("A comida favorita dele é " + comida_favorita)
+print("A cor favorita dele é " + cor_favorita)
---
+++ curso_python/aula9.py
@@ -0,0 +1,24 @@
+# Operadores aritméticos
+
+numero1 = 20
+numwor2 = 20
+
+primeira = numero1 + numwor2 # soma
+segunda = numero1 - numwor2 # subtração
+terceira = numero1 * numwor2 # multiplicação
+quarta = numero1 / numwor2 # divisão
+quinta = numero1 // numwor2 # divisão inteira
+sexta = numero1 % numwor2 # resto da divisão
+setima = numero1 ** numwor2 # exponenciação
+
+
+
+print(primeira) # soma
+print(segunda) # subtração
+print(terceira) # multiplicação
+print(quarta) # divisão
+print(quinta) # divisão inteira
+print(sexta) # resto da divisão
+print(setima) # exponenciação
+
+
