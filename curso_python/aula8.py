f8d0b25ce7d5f71958dd97461ff43675665eec36
Demetrios Alves <demetrios.10@hotmail.com>
2026-05-12 16:08:02 -0300

    update

---
+++ curso_python/aula8.py
@@ -0,0 +1,14 @@
+# exercicio de variaveis
+
+nome = "Demetrios"
+sobrenome = "Alves Da Silva"
+idade = 39
+ano_nascimento = 1986
+maior_de_idade = True
+altura_em_metros = 1.75
+
+print("O nome completo é " + nome + " " + sobrenome)
+print("Ele tem " + str(idade) + " anos")
+print("Ele nasceu em " + str(ano_nascimento))
+print("Ele é maior de idade: " + str(maior_de_idade))
+print("Ele mede " + str(altura_em_metros) + " metros")
