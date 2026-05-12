8ebf3809766194a2cbd463866c0cd8deeaf7a094
Demetrios Alves <demetrios.10@hotmail.com>
2026-05-12 15:48:59 -0300

    update

--- curso_python/aula6.py
+++ curso_python/aula6.py
@@ -1,5 +1,14 @@
 # coerção de tipos
 
-print(10 + 10.0) # o resultado é um float , pois o python converte o numero inteiro para um numero float
+
+# o resultado é um float , pois o python converte o numero inteiro para um numero float
+print(10 + 10.0)
 print(-10 + 20.9)
 
+print(30 + 30)
+print("D" + str(10)) # isso gera um erro , pois não é possível concatenar uma string com um numero inteiro
+print("D" + str("10")) # isso é possível , pois são duas strings
+
+print(int("1") , type("1"))
+print(float(1.7) , type(1.7))
+print(type(float("1.7")))
