e7bd6ae4ec2941764105d0eb9c15a265d4d8374a
Demetrios Alves <demetrios.10@hotmail.com>
2026-05-12 16:30:06 -0300

    Up

--- curso_python/aula9.py
+++ curso_python/aula9.py
@@ -11,8 +11,6 @@ quinta = numero1 // numwor2 # divisão inteira
 sexta = numero1 % numwor2 # resto da divisão
 setima = numero1 ** numwor2 # exponenciação
 
-
-
 print(primeira) # soma
 print(segunda) # subtração
 print(terceira) # multiplicação
@@ -20,5 +18,21 @@ print(quarta) # divisão
 print(quinta) # divisão inteira
 print(sexta) # resto da divisão
 print(setima) # exponenciação
+print(10 % 8 == 0) # isso é falso , pois o resto da divisão de 10 por 8 é 2 e não 0
+print(10 % 5 == 0) # isso é verdadeiro , pois o resto da
+
+
+
+
+joao_tem = 10
+maria_tem = 40
+
+print("A soma do valor dos dois é de: " + str(joao_tem + maria_tem)) # soma
+print("A subtração do valor dos dois é de: " + str(joao_tem - maria_tem)) # subtração
+print("A multiplicação do valor dos dois é de: " + str(joao_tem * maria_tem)) # multiplicação
+print("A divisão do valor dos dois é de: " + str(joao_tem / maria_tem)) # divisão
+print("A divisão inteira do valor dos dois é de: " + str(joao_tem // maria_tem)) # divisão inteira
+print("O resto da divisão do valor dos dois é de: " + str(joao_tem % maria_tem)) # resto da divisão
+
 
 
