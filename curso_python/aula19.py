8e5da44232666627af1942f38b2ee5b24ebb5b91
Demetrios Alves <demetrios.10@hotmail.com>
2026-05-28 16:34:07 -0300

    update

---
+++ curso_python/aula19.py
@@ -0,0 +1,14 @@
+
+# Operadores Logicos
+
+'''
+and 
+'''
+
+entrada = input("Digite [S]im ou [N]ao: ")
+senha = input("Digite a sua Senha: ")
+
+if entrada == "Sim" and senha == 95120:
+    print("Credenciais validas!!")
+else:
+    print("Credenciais Invalidas!!")
