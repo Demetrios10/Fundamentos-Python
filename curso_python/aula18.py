4f8252abe3ca52f98a155f35f65877f8a47a94cf
Demetrios Alves <demetrios.10@hotmail.com>
2026-05-28 14:59:55 -0300

    update

---
+++ curso_python/aula18.py
@@ -0,0 +1,28 @@
+# Opredaores de Comparação (relacionais)
+
+'''
+> maior
+< menor
+>= maior ou igual
+<= menor ou igual
+== igual
+!= diferente
+'''
+
+
+print(2 > 10)
+print(10 < 200)
+print(1000 >= 1000)
+print(200 <= 20)
+print(10 == 10)
+print("Demetrios" != 'Deltas')
+
+
+print('Deltas' == 'Deltassss')
+print('nome' != 'Nomes')
+
+
+
+
+
+
