f5e3c1bd6a4cb47b64336332bfb4c6312a402dfe
Demetrios Alves <demetrios.10@hotmail.com>
2026-06-14 17:10:13 -0300

    update

--- curso_python/aula24.py
+++ curso_python/aula24.py
@@ -1,10 +1,7 @@
 # interpolação basica de strings
 
-nome = input('Digite seu nome: ')
-print('Olá, ' + nome + '! Seja bem-vindo(a)!')
-# interpolação usando f-strings
-print(f'Olá, {nome}! Seja bem-vindo(a)!')
-# interpolação usando o método format()
-print('Olá, {}! Seja bem-vindo(a)!'.format(nome))
-# interpolação usando o operador %
-print('Olá, %s! Seja bem-vindo(a)!' % nome) 
+nome = input("Digite seu nome:")
+sobrenome = input("Digite seu sobrenome:")
+
+print(f"Ola, {nome} {sobrenome}!")
+print('Seja bem vindo!')
