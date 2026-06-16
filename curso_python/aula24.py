d05aef9cbbbf47641fcb01b606c8441163b9c134
Demetrios Alves <demetrios.10@hotmail.com>
2026-06-15 23:35:28 -0300

    update

--- curso_python/aula24.py
+++ curso_python/aula24.py
@@ -1,8 +1,4 @@
-# interpolação basica de strings
+nome = input('Digite seu nome: ')
+sobrenome = input('Digite seu sobrenome: ')
 
-nome = input("Digite seu nome:")
-sobrenome = input("Digite seu sobrenome:")
-
-print(f"Ola, {nome} {sobrenome}!")
-print('Seja bem vindo!')
-print(f'Seu nome é {nome} e seu sobrenome é {sobrenome}.')
+print(f'Olá {nome} {sobrenome}, seja bem-vindo(a)!')
