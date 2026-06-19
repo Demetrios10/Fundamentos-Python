c58569043307f0cf7f2b8f85ddbb23ebd9571c83
Demetrios Alves <demetrios.10@hotmail.com>
2026-06-18 23:49:22 -0300

    update

---
+++ curso_python/aula25.py
@@ -0,0 +1,14 @@
+# indices em python
+
+
+nome = 'Demetrios'
+print(nome[3]) 
+
+
+sobrenome = 'Alves da silva'
+print(sobrenome[4])
+print(sobrenome[0:3])
+print(sobrenome[2:5])
+print(len(sobrenome)) # conta os caracteres da string
+print(sobrenome[0:9:2]) # o terceiro número é o passo, ou seja, ele vai pegar um caractere a cada 2 caracteres
+print(sobrenome[::-1])# o primeiro número é o início, o segundo é o fim e o terceiro é o passo, ou seja, ele vai pegar um caractere a cada 3 caracteres
--- scripts_pessoais/Projeto-22.py
+++ scripts_pessoais/Projeto-22.py
@@ -1,7 +1,10 @@
-from matplotlib import pyplot as plt
+try:
+	from matplotlib import pyplot as plt
+except Exception:
+	raise SystemExit("Erro: não foi possível importar 'matplotlib'. Instale-o com: pip install matplotlib")
 
-# plotar gráfico de uma função 
-x = range(1,11,1)
-plt.plot(x,[(val**3+1)for val in x])
+# plotar gráfico de uma função
+x = range(1, 11, 1)
+plt.plot(x, [(val**3 + 1) for val in x])
 
 plt.show()
