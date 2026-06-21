f30b93249175389390510d6e6e33835716b5bb27
Demetrios Alves <demetrios.10@hotmail.com>
2026-06-21 18:20:19 -0300

    Update

--- curso_python/aula27.py
+++ curso_python/aula27.py
@@ -1,6 +1,16 @@
 # variaveis , constantes, complexibilidade de codigo
 
+# radar de velocidade
 
+velocidade = 200 # velocidade atual do carro
+local_carro = 200 # local onde o carro esta
+local_radar = 150 # local onde o radar esta
+velocidade_radar = 60 # velocidade permitida no radar
+
+if local_carro > local_radar and velocidade > velocidade_radar:
+    print('carro multado')
+else:
+    print('carro dentro do limite de velocidade')
 
 
 
