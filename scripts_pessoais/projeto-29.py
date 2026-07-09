c08430b429dfa1347c20263385c20eaed3ae7eae
Demetrios Alves <demetrios.10@hotmail.com>
2026-07-09 19:01:09 -0300

    Update

---
+++ scripts_pessoais/projeto-29.py
@@ -0,0 +1,17 @@
+'''
+1 - Faça um programa que peça para o usuario digitar um numero inteiro e informe
+se esse numero é par ou impar , caso o usuario digite um numero que não seja
+inteiro informe que não é um numero inteiro.
+
+
+
+2 - Faça um programa que pergunte a hora ao usuario e baseando-se no horario
+descrito , exiba uma saudação apropriada Ex:
+Bom dia 0-11, Boa tarde 12-17, Boa noite 18-23
+
+3 - Faça um programa que peça o primeiro nome de usuario .se o nome tiver
+4 letras ou menos escreva "Seu nome é curto"
+se tiver entre 5 e 6 letras escreva "Seu nome é normal"
+maior que 6 escreva "Seu nome é muito grande".
+
+'''
