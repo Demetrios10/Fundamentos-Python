ce6622214980dc0cc89a8c7a9d86e2bf71d03ada
Demetrios Alves <demetrios.10@hotmail.com>
2026-07-09 18:46:37 -0300

    Update

--- curso_python/aula29.py
+++ curso_python/aula29.py
@@ -3,3 +3,15 @@
 # is e is not = é ou não é (tipo , valor , identidade)
 # id = identidade do objeto (local na memória)
 
+condicao = None
+passou_no_if = True
+
+if passou_no_if:
+    print('faça algo')
+else:
+    print('não faça algo')
+
+if passou_no_if is None:
+    print('passou no if')
+else:
+    print('não passou no if')
