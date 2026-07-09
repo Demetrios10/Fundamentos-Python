8ad9e493b2f686e7861f3266c4a1413e822cc3b4
Demetrios Alves <demetrios.10@hotmail.com>
2026-07-09 18:27:49 -0300

    Update

--- curso_python/aula28.py
+++ curso_python/aula28.py
@@ -1,4 +1,6 @@
-# Aula 28 - Funções com Parâmetros Padrão
+# Id A identidade de um elemento na memoria é dada pela função id()
+# que retorna um número inteiro único para cada objeto criado na memória.
+# Esse número é o endereço de memória do objeto.
 
 
 
